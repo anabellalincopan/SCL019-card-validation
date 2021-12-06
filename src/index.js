f833557a97f9a38745c2b654e3184d2d4b35c385
Anabella Lincopan <anabella.lincopan@ug.uchile.cl>
2021-12-06 15:31:05 -0400

    entrar

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 import validator from './validator.js';
 
-console.log(validator);
+const numCard= document.getElementById('ncard');
+const numCode= document.getElementById('ncode');
+const validButton= document.getElementsById('validbtn');
+const restartButton= document.getElementsById('restartbtn');
+const alertBox= document.getElementsByClassName('alert-box');
+const validCard= document.getElementsByClassName('valid');
+const novalidCard= document.getElementsByClassName('no-valid');
+
+
+// función para no dejar input vacio
+validateNumCard(numCard);
+function validateNumCard(numCard){
+    while (numCard==''|| numCard==null){
+        numCard= alert('Ingrese un número de tarjeta');
+    }
+
+    return 0;
+}
+
+// función para permitir solo números en input
+
+// botón para validar tarjeta
+
+// botón para reiniciar
+
+// llamar a función is.valid
+
+// llamar a función maskify
+
+
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,22 @@
 const validator = {
-  // ...
+
+  //FÓRMULA DE LUHN
+
+  // pasar array a orden inverso
+
+  // aplicar operación a números en posiciones pares
+
+  // sumar dígitos 
+
+  // validar tarjeta
+
+
+  // MASKIFY
+
+  
+
+
+
 };
 
 export default validator;
