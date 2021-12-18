009a71bc6c24b1b5689b9ae1a3505f8be156b319
Anabella Lincopan <anabella.lincopan@ug.uchile.cl>
2021-12-17 22:42:19 -0400

    entrar

--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
 import validator from './validator.js';
 
-const numCard= document.getElementById('ncard');
-const numCode= document.getElementById('ncode');
-const validButton= document.getElementsById('validbtn');
-const restartButton= document.getElementsById('restartbtn');
-const alertBox= document.getElementsByClassName('alert-box');
-const validCard= document.getElementsByClassName('valid');
-const novalidCard= document.getElementsByClassName('no-valid');
+// const numCode= document.getElementById('ncode');
+// const validButton= document.getElementById('validbtn');
+// const restartButton= document.getElementById('restartbtn');
+// const alertBox= document.getElementByClassName('alert-box');
+// const validCard= document.getElementByClassName('valid');
+// const novalidCard= document.getElementByClassName('no-valid');
 
 
 // función para no dejar input vacio
-validateNumCard(numCard);
-function validateNumCard(numCard){
-    while (numCard==''|| numCard==null){
-        numCard= alert('Ingrese un número de tarjeta');
-    }
 
-    return 0;
+function validateNumCard(){
+    const numCard= document.getElementById('ncard').value;
+    if (numCard==''|| numCard===null){
+    alert('Ingrese un número de tarjeta');
+    }
+    console.log(numCard)
+
 }
+document.getElementById('validbtn').addEventListener('click', validateNumCard, false);
 
 // función para permitir solo números en input
 
--- src/validator.js
+++ src/validator.js
@@ -4,19 +4,56 @@ const validator = {
 
   // pasar array a orden inverso
 
+  isValid: function (ncard){
+    const inverseNumber= ncard.toString().split('').reverse().map(Number);
+    console.log(inverseNumber)
+    
+  }
+
+
   // aplicar operación a números en posiciones pares
+  // let newNumber= inverseNumber.map((num, i) => {
+  //   if (i % 2 === 1) {
+  //     const double = (num * 2);
+  //     const m = double.toString().split('').reduce(function (a, b) {
+  //       if (double > 9) {
+  //         return a + parseInt(b);
+  //       } else {
+  //         return double;
+  //       }
+  //     }, 0);
+  //     return m;
+  //   } else {
+  //      return num;
+  //    }
+  //  }
 
-  // sumar dígitos 
+  // let pairNumber = pairArray.filter(
+  //   function (num, ind) { return ind % 2 !== 0; })
+  // let multiply= digitosPares.map(function (num) { return num * 2 });
 
-  // validar tarjeta
+  // sumar dígitos
+
+  // let newNumber= newNumber.reduce((previus, next) => {
+  //   const sum = previus + next;
+  //   return sum;
+  //   }, 0)
+
+  // // validar tarjeta
+  // if (newNumber % 10 === 0) {
+  //   return true;
+  // } else {
+  //   return false;
+  // }
+
 
 
   // MASKIFY
 
-  
 
 
 
-};
 
+
+}
 export default validator;
