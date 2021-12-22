8e3b242ed1a962b144aee6f79e01fd9d7b7f6b9a
Anabella Lincopan <anabella.lincopan@ug.uchile.cl>
2021-12-21 23:22:20 -0400

    enter

--- src/index.js
+++ src/index.js
@@ -3,30 +3,41 @@ import validator from './validator.js';
 // const numCode= document.getElementById('ncode');
 // const validButton= document.getElementById('validbtn');
 // const restartButton= document.getElementById('restartbtn');
-// const alertBox= document.getElementByClassName('alert-box');
-// const validCard= document.getElementByClassName('valid');
-// const novalidCard= document.getElementByClassName('no-valid');
-
-
-// función para no dejar input vacio
 
 function validateNumCard(){
     const numCard= document.getElementById('ncard').value;
     if (numCard==''|| numCard===null){
     alert('Ingrese un número de tarjeta');
+    return false;
     }
-    console.log(numCard)
+    // console.log(numCard);
+
+    // validar tarjeta
+    const Validate= validator.isValid(numCard);
+    const alertBox= document.getElementsByClassName('alert-box');
+    const validCard= document.getElementsByClassName('valid');
+    const noValidCard= document.getElementsByClassName('no-valid');
+    for (let i = 0; i < alertBox.length;i++) {
+        if (Validate === true){
+            alertBox[i].style.display= 'block';
+            noValidCard[i].style.display= 'none';
+            validCard[i].style.display= 'block';
+        } else {
+            alertBox[i].style.display= 'block';
+            noValidCard[i].style.display= 'block';
+            validCard[i].style.display= 'none';
+        }
+    }
+
 
 }
 document.getElementById('validbtn').addEventListener('click', validateNumCard, false);
 
 // función para permitir solo números en input
 
-// botón para validar tarjeta
 
 // botón para reiniciar
 
-// llamar a función is.valid
 
 // llamar a función maskify
 
--- src/validator.js
+++ src/validator.js
@@ -2,55 +2,53 @@ const validator = {
 
   //FÓRMULA DE LUHN
 
-  // pasar array a orden inverso
 
-  isValid: function (ncard){
-    const inverseNumber= ncard.toString().split('').reverse().map(Number);
-    console.log(inverseNumber)
-    
+
+  isValid: function(numCard) {
+    // pasar array a orden inverso
+    let inverseNumber= numCard.toString().split('').reverse().map(Number);
+    // console.log(inverseNumber);
+
+
+    // aplicar operación a números en posiciones pares
+      let newNumber= inverseNumber.map((num, i)=> {
+        if(i % 2 === 1){
+          // a todos los numeros que ocupan las posiciones pares se les multiplica por dos
+          let multiply = (num * 2);
+          //y si el doble de ese numero es mayor a 9 debemos sumar los digitos del resultado
+          let digitSum= multiply.toString().split('').map(Number).reduce(function(a, b){
+            if (multiply > 9){
+              return a+b;
+            // console.log(multiply)
+          } else {
+            return multiply;
+          }
+          },0);
+          return digitSum;
+          // console.log(digitSum)
+        } else {
+          return num;
+        }
+      });
+      //console.log(newNumber)
+
+       // sumar dígitos
+      newNumber= newNumber.reduce((prev, next) => {
+        return prev + next;
+      },0)
+      // console.log(newNumber)
+
+      // validar fórmula
+      if (newNumber % 10 === 0) {
+        return true;
+      } else {
+        return false;
+      }
   }
-
-
-  // aplicar operación a números en posiciones pares
-  // let newNumber= inverseNumber.map((num, i) => {
-  //   if (i % 2 === 1) {
-  //     const double = (num * 2);
-  //     const m = double.toString().split('').reduce(function (a, b) {
-  //       if (double > 9) {
-  //         return a + parseInt(b);
-  //       } else {
-  //         return double;
-  //       }
-  //     }, 0);
-  //     return m;
-  //   } else {
-  //      return num;
-  //    }
-  //  }
-
-  // let pairNumber = pairArray.filter(
-  //   function (num, ind) { return ind % 2 !== 0; })
-  // let multiply= digitosPares.map(function (num) { return num * 2 });
-
-  // sumar dígitos
-
-  // let newNumber= newNumber.reduce((previus, next) => {
-  //   const sum = previus + next;
-  //   return sum;
-  //   }, 0)
-
-  // // validar tarjeta
-  // if (newNumber % 10 === 0) {
-  //   return true;
-  // } else {
-  //   return false;
-  // }
 
 
 
   // MASKIFY
-
-
 
 
 
